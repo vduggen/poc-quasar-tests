6305d4c333efd5c4efa25306f25e725844ccf5cb
Vitor Duggen <zduggem@gmail.com>
2023-01-13 14:17:46 -0300

    Criado 2 novos testes

--- src/tests/app.test.ts
+++ src/tests/app.test.ts
@@ -2,7 +2,7 @@ import { mount } from '@vue/test-utils';
 import { useQuasar, Quasar } from 'quasar';
 import App from "../App.vue"
 
-describe('Deve testar a tela principal', () => {
+describe('<App />', () => {
     it('Caso não tiver nenhum usuário buscado deve estar no estado de vazio', () => {
         const wrapper = mount(App, {
             global: {
@@ -13,5 +13,44 @@ describe('Deve testar a tela principal', () => {
             }
         });
         expect(wrapper.find('[data-test=emptyState').exists()).toBe(true);
+    })
+
+    it('Caso for buscado um usuário deve sumir o estado de vazio', async () => {
+        const wrapper = mount(App, {
+            global: {
+                plugins: [Quasar],
+                provide: {
+                    quasar: useQuasar()
+                }
+            }
+        });
+        const usernameInput = wrapper.find('[data-test=username]');
+        usernameInput.setValue('vduggen');
+        const buttonFetch = wrapper.find('[data-test=buttonFetch]');
+        buttonFetch.trigger('click');
+        await new Promise((resolve) => {
+            setTimeout(() => resolve(''), 300);
+        })
+        expect(wrapper.find('[data-test=emptyState]').exists()).toBe(false);
+    })
+
+    it('Caso for buscado um usuário deve mostrar os componentes de abas e de informações do usuário', async () => {
+        const wrapper = mount(App, {
+            global: {
+                plugins: [Quasar],
+                provide: {
+                    quasar: useQuasar()
+                }
+            }
+        });
+        const usernameInput = wrapper.find('[data-test=username]');
+        usernameInput.setValue('vduggen');
+        const buttonFetch = wrapper.find('[data-test=buttonFetch]');
+        buttonFetch.trigger('click');
+        await new Promise((resolve) => {
+            setTimeout(() => resolve(''), 300);
+        })
+        expect(wrapper.find('[data-test=tabs]').exists()).toBe(true);
+        expect(wrapper.find('[data-test=wrapperUserInfo]').exists()).toBe(true);
     })
 })
